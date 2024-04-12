fe2746bda9d39072851fe464de6327ea8f33dab8
YuQi_Zhou <rdjksp@gmail.com>
2024-04-12 20:42:31 +0800

    编写了正规文法转换为dfa的方法

--- src/regular_grammar.rs
+++ src/regular_grammar.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use std::f32::consts::E;
+use crate::deterministic_finite_automaton::{DeterministicFiniteAutomaton, TransFunc};
 
 #[derive(Debug, Clone)]
 pub struct RegularGrammar {
@@ -10,7 +11,7 @@ pub struct RegularGrammar {
 }
 
 impl RegularGrammar {
-    pub fn parse_grammar_token_and_build(grammars: String,start:char) -> Result<RegularGrammar, String> {
+    pub fn parse_grammar_token_and_build(grammars: String, start: char) -> Result<RegularGrammar, String> {
         let mut builder = RegularGrammar {
             terminal: Default::default(),
             non_terminal: Default::default(),
@@ -31,15 +32,15 @@ impl RegularGrammar {
                         return Err("该文法不是正规文法".into_string());
                     }
                     builder.production_set.insert(left_vn.clone(), right_production.into_string());
-                    let mut char_iter=right_production.chars();
-                    if let Some(v_t)=char_iter.next(){
-                        if builder.non_terminal.contains(&v_t){
+                    let mut char_iter = right_production.chars();
+                    if let Some(v_t) = char_iter.next() {
+                        if builder.non_terminal.contains(&v_t) {
                             return Err("重复的终结符和非终结符".into_string());
                         }
                         builder.terminal.insert(v_t);
                     }
-                    if let Some(v_n)=char_iter.next(){
-                        if builder.terminal.contains(&v_n){
+                    if let Some(v_n) = char_iter.next() {
+                        if builder.terminal.contains(&v_n) {
                             return Err("重复的终结符和非终结符".into_string());
                         }
                         builder.non_terminal.insert(v_n)
@@ -51,7 +52,22 @@ impl RegularGrammar {
         }
         Ok(builder)
     }
-    pub fn into_dfa(self){
-
+    pub fn into_dfa(self) -> Result<DeterministicFiniteAutomaton, ()> {
+        let alpha = self.terminal;
+        let mut states = self.non_terminal;
+        states.insert('+');// '+'即结束状态
+        let mut end_state = HashSet::new();
+        end_state.insert('+');
+        let start_state = self.start;
+        let trans = self.production_set.into_iter().map(|(v_n, sen)| {
+            let mut char_iter = sen.chars();
+            let left_vt = char_iter.next().unwrap();
+            if let Some(right_v_n) = char_iter.next() {
+                (TransFunc::new(v_n, left_vt), right_v_n)
+            } else {
+                (TransFunc::new(v_n, left_vt), '+')//转移到接受状态
+            }
+        }).collect::<HashMap<_, _>>();
+        DeterministicFiniteAutomaton::build(alpha, states, start_state, end_state, trans)
     }
 }
