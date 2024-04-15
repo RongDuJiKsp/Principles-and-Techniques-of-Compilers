0d1caeaad2629f0e7e6de6bacd65cc25becfda86
YuQi_Zhou <rdjksp@gmail.com>
2024-04-15 14:41:04 +0800

    nya~

--- src/pushdown_automaton_grammar.rs
+++ src/pushdown_automaton_grammar.rs
@@ -1,5 +1,6 @@
 use std::collections::{HashMap, HashSet};
 use crate::deterministic_finite_automaton::State;
+use crate::utils::split_type_two_grammar;
 
 #[derive(Debug, Clone)]
 pub struct PushDownAutomatonGrammar {
@@ -14,8 +15,10 @@ impl PushDownAutomatonGrammar {
         if !start_state.is_ascii_uppercase() {
             return Err("不是按照传统约束的合法状态！".to_string());
         }
-        for grammar_sen in grammar_tokens.split(","){
+        for grammar_sen in grammar_tokens.split(",") {
+            if let Ok((left_vn, right_sense)) = split_type_two_grammar(grammar_sen.to_string()) {
 
+            }
         }
         Err("".to_string())
     }
